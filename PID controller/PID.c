8efb2abfcafbb027b9cd06867506a47e97ed6f11
Dorota Chudzik <dorota_chudzik@wp.pl>
2017-12-26 10:13:05 +0100

    biblioteka regulatora PID

---
+++ PID controller/PID.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+typdef struct  {
+	float kp ; //gain  P (proportional term)
+    float ki;  //gain I (integral term )
+    float kd;  //gain D (derivative term)
+    float dt;
+    float LastError;
+    float Integral;
+	float error;
+}PID
+
+PIDs[]=[PID PID1, PID PID2, PID PID3, PID PID4]; 
+
+void PID_init(nr, kp, ki, kd, dt):
+{
+	PIDs[nr].kp = kp;  
+    PIDs[nr].ki = ki;   
+    PIDs[nr].kd = kd;  
+    PIDs[nr].dt = dt; 
+    PIDs[nr].Integral = 0;
+    PIDs[nr].LastError = 1;
+	PIDs[nr].error = 0;
+	return 0;
+}  
+
+float PID_update(nr, desired, measured):
+{
+    
+	filter=1/(1+PID.s[nr].dt)
+	error = desired - measured;
+    p = PIDs[nr].kp * error;
+    PIDs[nr].Integral += (error + PIDs[nr].LastError) * PIDs[nr].dt * 0.5;
+    d = ( (error - PIDs[nr].LastError)/PIDs[nr].dt) * PIDs[nr].kd;
+
+    PIDs[nr].LastError = error;
+
+    return p + PIDs[nr].Integral*PIDs[nr].ki + d; 
+}
+//low-pass filter - 
+//define values
