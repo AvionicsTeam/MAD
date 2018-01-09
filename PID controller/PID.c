2ccb0b39c1c17237c407d1a5e4e8001716f92f87
Dorota Chudzik <dorota_chudzik@wp.pl>
2018-01-09 22:51:03 +0100

    aktualizacja PID

--- PID controller/PID.c
+++ PID controller/PID.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
 
 typdef struct  {
-	float kp ; //gain  P (proportional term)
-    float ki;  //gain I (integral term )
-    float kd;  //gain D (derivative term)
-    float dt;
-    float LastError;
-    float Integral;
-	float error;
+	float kp ;
+	float ki;
+	float kd;
+	float dt;
+	float LastError;
+	float Integral;
+	float windup;
 }PID
 
-PIDs[]=[PID PID1, PID PID2, PID PID3, PID PID4]; 
+PIDs[]=[PID PID1, PID PID2, PID PID3, PID PID4];
 
-void PID_init(nr, kp, ki, kd, dt):
+void PID_init(int nr, float kp, float ki, float kd, float dt, float windup)
 {
-	PIDs[nr].kp = kp;  
-    PIDs[nr].ki = ki;   
-    PIDs[nr].kd = kd;  
-    PIDs[nr].dt = dt; 
-    PIDs[nr].Integral = 0;
-    PIDs[nr].LastError = 1;
-	PIDs[nr].error = 0;
+	PIDs[nr].kp = kp;
+	PIDs[nr].ki = ki;
+	PIDs[nr].kd = kd;
+	PIDs[nr].dt = dt* 0.5;
+	PIDs[nr].Integral = 0;
+	PIDs[nr].LastError = 1;
+	PIDs[nr].windup = windup;
+
+
 	return 0;
-}  
+}
 
-float PID_update(nr, desired, measured):
+float PID_update(nr, desired, measured)
 {
-    
 	
 	error = desired - measured;
-    p = PIDs[nr].kp * error;
-    PIDs[nr].Integral += (error + PIDs[nr].LastError) * PIDs[nr].dt * 0.5;
-    d = ( (error - PIDs[nr].LastError)/PIDs[nr].dt) * PIDs[nr].kd;
+	p = PIDs[nr].kp * error;
+	PIDs[nr].Integral += (error + PIDs[nr].LastError) * PIDs[nr].dt ;
+	//ANTIWINDUP
+	if(PIDs[nr].windup != 0)
+	{
+		if(PIDs[nr].Integral>PIDs[nr].windup)
+			PIDs[nr].Integral = PIDs[nr].windup;
+		if(PIDs[nr].Integral<-PIDs[nr].windup)
+			PIDs[nr].Integral = -PIDs[nr].windup;
+	}
+	d = ( (error - PIDs[nr].LastError)/PIDs[nr].dt) * PIDs[nr].kd;
 
-    PIDs[nr].LastError = error;
+	PIDs[nr].LastError = error;
 
-    return p + PIDs[nr].Integral*PIDs[nr].ki + d; 
+	return p + PIDs[nr].Integral*PIDs[nr].ki + d;
 }
-//low-pass filter - 
-//define values
