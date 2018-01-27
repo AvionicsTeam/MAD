a84cdbd6f30b49c562cf1c4e93d1fbbc40e0100d
Dorota Chudzik <dorota_chudzik@wp.pl>
2018-01-27 16:39:17 +0100

    Aktualizacja testów funkcji PID

--- PID controller/PID/test.c
+++ PID controller/PID/test.c
@@ -3,8 +3,8 @@
 
 void init_test()
 {
-	PID_init(0, 0.000478, 0.00478, 0, 0.02, 0);
-	PID_init(1,0.0004, 0.004,0, 0.02, 0);
+	PID_init(0, 0.000478, 0.0478, 0, 0.02, 0);
+	PID_init(1, 0.00057, 0.057,0, 0.02, 0);
 	PID_init(2, 0.012, 0.12, 0, 0.02, 0);
 	PID_init(3, 0.012, 0.12, 0, 0.02, 0);
 }
@@ -16,9 +16,4 @@ float test(int nr, float des, float meas)
 	
 	return y;
 
-}
-
-float test_test(float a)
-{
-	return 15.0;
 }
--- PID controller/PID/test.h
+++ PID controller/PID/test.h
@@ -1,3 +1,2 @@
 void init_test();
 float test(int nr, float des, float meas);
-float test_test(float a);
